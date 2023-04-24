9498cc07ef04738989738e78a58313b5c3b8912b
letdtcode <boynguyenducthanh@gmail.com>
2023-04-25 00:58:44 +0700

    handle tablayout + viewpager in order fragment

---
+++ app/src/main/java/com/iostar/beverageshop/adapter/ViewPagerOrderAdapter.java
@@ -0,0 +1,28 @@
+package com.iostar.beverageshop.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPagerOrderAdapter extends FragmentStateAdapter {
+    private ArrayList<Fragment> fragments;
+
+    public ViewPagerOrderAdapter(@NonNull FragmentActivity fragmentActivity, ArrayList<Fragment> fragments) {
+        super(fragmentActivity);
+        this.fragments = fragments;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
+}
--- app/src/main/java/com/iostar/beverageshop/fragment/user/OrderFragment.java
+++ app/src/main/java/com/iostar/beverageshop/fragment/user/OrderFragment.java
@@ -12,15 +12,26 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.iostar.beverageshop.activity.user.CartActivity;
+import com.iostar.beverageshop.adapter.ViewPagerOrderAdapter;
 import com.iostar.beverageshop.databinding.FragmentHomeBinding;
 import com.iostar.beverageshop.databinding.FragmentOrderBinding;
+import com.iostar.beverageshop.fragment.user.order.OrderCanceledFragment;
+import com.iostar.beverageshop.fragment.user.order.OrderSuccessFragment;
+import com.iostar.beverageshop.fragment.user.order.OrderWaitingConfirmFragment;
+import com.iostar.beverageshop.fragment.user.order.OrderWaitingDeliveryFragment;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class OrderFragment extends Fragment {
     private FragmentOrderBinding binding;
+    private ArrayList<Fragment> fragmentOrderDetail;
+    private ViewPagerOrderAdapter viewPagerOrderAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,7 +43,50 @@ public class OrderFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        initial();
         setEvent();
+    }
+
+    private void initial() {
+        fragmentOrderDetail = new ArrayList<>();
+        fragmentOrderDetail.add(new OrderWaitingConfirmFragment());
+        fragmentOrderDetail.add(new OrderWaitingDeliveryFragment());
+        fragmentOrderDetail.add(new OrderSuccessFragment());
+        fragmentOrderDetail.add(new OrderCanceledFragment());
+
+//        ViewPagerOrderAdapter viewPagerOrderAdapter = new ViewPagerOrderAdapter(this, fragmentOrderDetail);
+//        binding.viewPagerTabLayout.setAdapter(viewPagerOrderAdapter);
+////        binding.viewPagerTabLayout.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+////            @Override
+////            public void onPageSelected(int position) {
+////                switch (position) {
+////                    case 0:
+////                        binding.tabLayout.setsele
+////                }
+////            }
+////        });
+        viewPagerOrderAdapter = new ViewPagerOrderAdapter(getActivity(), fragmentOrderDetail);
+        binding.viewPagerTabLayout.setAdapter(viewPagerOrderAdapter);
+        new TabLayoutMediator(binding.tabLayout, binding.viewPagerTabLayout, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+                    case 0:
+                        tab.setText("Chờ xác nhận");
+                        break;
+                    case 1:
+                        tab.setText("Đang giao");
+                        break;
+                    case 2:
+                        tab.setText("Đã nhận");
+                        break;
+                    case 3:
+                        tab.setText("Đã hủy");
+                        break;
+                }
+            }
+        }).attach();
     }
 
     private void setEvent() {
